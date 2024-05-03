384d175641acec74106a200da25a56d31d311393
Trongthuy2306 <thuynguyen.23062002@gmail.com>
2024-05-03 13:51:53 +0700

    add CRUD for server

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,11 +1,12 @@
+import { ServerFeatureTodoModule } from '@fst/server/feature-todo';
 import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ServerFeatureTodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ libs/server/feature-todo/src/lib/dtos/todo.dto.ts
@@ -0,0 +1,44 @@
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { ITodo } from '@fst/shared/domain';
+
+export class CreateTodoDto implements Pick<ITodo, 'title' | 'description'> {
+    @IsString()
+    @IsNotEmpty()
+    title!: string;
+
+    @IsString()
+    @IsNotEmpty()
+    description!: string;
+}
+
+export class UpsertTodoDto implements ITodo {
+    @IsString()
+    @IsNotEmpty()
+    title!: string;
+
+    @IsString()
+    @IsNotEmpty()
+    description!: string;
+
+    @IsString()
+    @IsNotEmpty()
+    id!: string;
+
+    @IsBoolean()
+    @IsNotEmpty()
+    completed!: boolean;
+}
+
+export class UpdateTodoDto implements Partial<Omit<ITodo, 'id'>> {
+    @IsString()
+    @IsOptional()
+    title!: string;
+
+    @IsString()
+    @IsOptional()
+    description!: string;
+
+    @IsBoolean()
+    @IsOptional()
+    completed!: boolean;
+}
